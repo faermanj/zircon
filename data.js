a133aaf59f552dd8cc1a763a38926a0095c78bbd
Julio Faerman <jfaerman@gmail.com>
2017-07-17 09:29:01 -0300

    Add jshint

--- data.js
+++ data.js
@@ -2,6 +2,7 @@
 
 const urls = {
   'gist' : 'https://gist.github.com/jfaerman/546902c79d21e53c3a724d00a30566f2',
+  'zircon':'https://github.com/jfaerman/zircon',
   'sec-job0': 'https://www.youtube.com/watch?v=T7MnJOfOVcY',
   'sec-overview': 'https://d0.awsstatic.com/whitepapers/aws-security-whitepaper.pdf',
   'sec-minicon': 'https://www.youtube.com/watch?v=8ZljcKn8FPA',
